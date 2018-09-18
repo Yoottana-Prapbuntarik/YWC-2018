1478157559cb8a626eedddf703980145b3d5e31f
Devtool <email>
2018-09-18 15:23:46 +0700

    จงเขียน function cardAt ที่รับค่า n และคืนค่าเป็นเลขของไพ่ในสำรับลำดับที่ n โดยเรียงตามดอกไพ่ (C, D, H, S) และ

---
+++ cardAt.js
@@ -0,0 +1,34 @@
+/*
+จงเขียน function cardAt ที่รับค่า n และคืนค่าเป็นเลขของไพ่ในสำรับลำดับที่ n โดยเรียงตามดอกไพ่ (C, D, H, S) และ
+เลขไพ่ (2, 3, 4, 5, 6, 7, 8, 9, 0, J, Q, K, A) ตามลำดับ 
+ตัวอย่างเช่น 
+cardAt(0) = 2C, cardAt(1) = 3C, cardAt(34) = 0H และ cardAt(35) = JH 
+เป็นต้น โดยเขียนคำตอบมาโดยใช้ภาษาใดก็ได้ หรือเป็น psuedocode ก็ได้เช่นกัน
+*/ 
+let ans;
+let n;
+let at = ['C','D','H','S'];
+function cardAt (Number){
+let card = ['2','3','4','5','6','7','8','9','0','J','Q','K','A'];
+ans = card[Number];
+return ans;
+}
+
+var readline = require('readline-sync');
+
+n = readline.question('input number : ');
+if((n >= 0 ) && (n <= 12)){
+    console.log(cardAt(n) + at[0]);
+}
+
+else if((n > 12 ) && (n <= 25)) {
+    console.log(cardAt(n-13) + at[1]);
+}
+
+else if((n > 25 ) && (n <= 38)) {
+    console.log(cardAt(n-26) + at[2]);
+}
+
+else if((n > 38 ) && (n <= 51)) {
+    console.log(cardAt(n-39) + at[3]);
+}
